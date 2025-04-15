fff3c025a22e2111a97a7ea495c39dc191febe1b
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2025-04-15 16:15:06 +0200

    Setup project

---
+++ app/layout.jsx
@@ -0,0 +1,40 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import clsx from "clsx";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Auto dépannage romanais",
+  description: "Auto depannage romanais website",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="h-full" suppressHydrationWarning>
+      <body
+        className={clsx(
+          "h-full bg-background font-sans antialiased",
+          geistSans.variable,
+          geistMono.variable
+        )}
+        cz-shortcut-listen="true"
+      >
+        {/* <Header /> */}
+
+        <main className="min-h-full mx-auto max-w-4xl px-4 mt-4 lg:mt-6 flex flex-col gap-4">
+          {children}
+        </main>
+        {/* <Footer /> */}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <h1 className="font-bold text-4xl text-center text-primary">ADR</h1>
+    </main>
+  );
+}
