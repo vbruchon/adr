217e9549cbffbc342c0cd2c7b2d217caf1bc90de
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2025-04-28 14:07:57 +0200

    update service icon render with useIsCLient hook

--- app/components/services-banner.jsx
+++ app/components/services-banner.jsx
@@ -1,17 +1,14 @@
 "use client";
 
-import { useTheme } from "next-themes";
 import { SERVICES } from "../../src/data";
-import Image from "next/image";
 import Link from "next/link";
+import { ThemedIcon } from "@/src/components/theme/themed-icon";
 
 export const ServicesBanner = () => {
-  const { theme } = useTheme();
   return (
     <section className="py-16 px-4">
       <h2 className="text-3xl font-semibold text-center mb-8">Nos Services</h2>
 
-      {/* Aperçu des services */}
       <div className="mx-auto flex items-center justify-center flex-wrap gap-8 ">
         {SERVICES.map((service) => (
           <Link
@@ -19,12 +16,7 @@ export const ServicesBanner = () => {
             key={service.name}
             className="bg-card flex flex-col items-center gap-2 p-4 border rounded-xl w-40 hover:bg-card-foreground"
           >
-            <img
-              width={64}
-              height={64}
-              src={theme === "dark" ? service.icon : service.iconDark}
-              alt=""
-            />
+            <ThemedIcon item={service} />
             <p>{service.name}</p>
           </Link>
         ))}
---
+++ src/components/theme/themed-icon.jsx
@@ -0,0 +1,25 @@
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import { useIsClient } from "@/src/hooks/use-is-client";
+import { Loader } from "lucide-react";
+
+export const ThemedIcon = ({ item }) => {
+  const { theme } = useTheme();
+  const isClient = useIsClient();
+
+  return (
+    <>
+      {isClient ? (
+        <Image
+          width={64}
+          height={64}
+          src={theme === "dark" ? item.icon : item.iconDark}
+          alt={item.name}
+          loading="lazy"
+        />
+      ) : (
+        <Loader className="animate-spin" />
+      )}
+    </>
+  );
+};
