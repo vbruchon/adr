47d27966f390e22d009fbb2f68e92e617f1c7d45
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2025-05-23 17:05:25 +0200

    fix: display correct icon in dark mode

--- src/components/theme/themed-icon.jsx
+++ src/components/theme/themed-icon.jsx
@@ -1,25 +1,18 @@
 import { useTheme } from "next-themes";
 import Image from "next/image";
-import { useIsClient } from "@/src/hooks/use-is-client";
-import { Loader } from "lucide-react";
 
 export const ThemedIcon = ({ item }) => {
   const { theme } = useTheme();
-  const isClient = useIsClient();
 
   return (
     <>
-      {isClient ? (
-        <Image
-          width={64}
-          height={64}
-          src={theme === "dark" ? item.icon : item.iconDark}
-          alt={`Icône représentant un service de ${item.name}`}
-          loading="lazy"
-        />
-      ) : (
-        <Loader className="animate-spin" />
-      )}
+      <Image
+        width={64}
+        height={64}
+        src={theme === "dark" ? item.iconDark : item.icon}
+        alt={`Icône représentant un service de ${item.name}`}
+        loading="lazy"
+      />
     </>
   );
 };
